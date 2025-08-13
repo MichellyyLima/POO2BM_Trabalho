9748d8b200075e4a2af81f5420481a810573cdc9
MichellyyLima <michellyaugustasilvadelima@gmail.com>
2025-08-13 08:26:24 -0300

    13/08
    ajustes

---
+++ PROJETO.POO2BM/Assets/SCRIPT/ControledeAudio.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
+public class ControledeAudio : MonoBehaviour
+{
+    public int Volume;
+    public AudioMixer volumeSFX;
+    public bool sound;
+    
+    public Toggle toggleSound;
+        
+        
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
