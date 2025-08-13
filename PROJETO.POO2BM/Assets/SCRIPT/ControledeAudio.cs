4afa79b99a051bfe2bba79301a589837056dd0e5
MichellyyLima <michellyaugustasilvadelima@gmail.com>
2025-08-13 11:53:26 -0300

    13/08
    continuação dos trabalhos

--- PROJETO.POO2BM/Assets/SCRIPT/ControledeAudio.cs
+++ PROJETO.POO2BM/Assets/SCRIPT/ControledeAudio.cs
@@ -1,25 +1,48 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Rendering;
 using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
+using Toggle = UnityEngine.UI.Toggle;
+
 public class ControledeAudio : MonoBehaviour
 {
     public int Volume;
-    public AudioMixer volumeSFX;
-    public bool sound;
+    public int VolumeSFX;
+    public bool musica;
     
-    public Toggle toggleSound;
-        
-        
+    
+    public Slider volumeSlider;
+    public Slider volumeSFXSlider;
+    public Toggle toggleMusica;
+    public TMP_Text textomusica; 
+    
+    
     void Start()
     {
-        
+        musica = toggleMusica.isOn;
+        Volume = (int)volumeSFXSlider.value;
+        VolumeSFX = (int)volumeSFXSlider.value;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        musica = toggleMusica.isOn;
+        Volume = (int)volumeSFXSlider.value;
+        VolumeSFX = (int)volumeSFXSlider.value;
+
+        if (musica == true)
+        {
+          textomusica.text = "Ligado";
+          textomusica.color = Color.magenta;
+        }
+        else
+        {
+            textomusica.text = "Desligado";
+            textomusica.color = Color.yellow;
+        }
+            
     }
 }
 
